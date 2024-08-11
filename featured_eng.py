1de4cbcb941cc091e8453fb45b24d59c4dc1e750
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2024-08-11 20:17:53 +1000

    final

--- featured_eng.py
+++ featured_eng.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-# Load the normalized dataset
 data = pd.read_csv('./normalized_data.csv')
 
-# Creating composite features
+
 data['AT_V'] = data['AT'] * data['V']
 data['AT_AP'] = data['AT'] * data['AP']
 data['V_RH'] = data['V'] * data['RH']
